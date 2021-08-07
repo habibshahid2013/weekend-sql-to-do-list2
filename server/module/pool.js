197006e6b10e62e44f9368fcaa9a5119126ac3ff
Hassan Shahid <habibshahid2013@gmail.com>
2021-08-07 09:52:49 -0500

    added changes to pool.js

--- server/module/pool.js
+++ server/module/pool.js
@@ -1,11 +1,21 @@
-const express = require('express');
-const db = require('../modules/pool');
-const router = express.Router();
+const pg = require('pg');
 
+const config = {
+    database: 'weekend-to-do-app',
+    host: 'localhost',
+    port: 5432,
+    max: 10,
+    idleTimeoutMillis: 30000
+}
 
+const pool = new pg.Pool(config);
 
+pool.on('connect', () => {
+    console.log('DB Connection Successful.');
+});
 
+pool.on("error", (error) => {
+    console.log('DB Connection Error:', error);
+});
 
-
-
-module.exports = router;
+module.exports = pool;
